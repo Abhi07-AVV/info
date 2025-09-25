6486cedd7ccafbee65640efcf51e5489fb2a46b1
Abhi07-AVV <abhinavin07@gmail.com>
2025-09-25 21:19:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
     plugins: [react()],
     resolve: {
@@ -10,19 +10,6 @@ export default defineConfig(({ command }) => {
         '@': path.resolve(__dirname, './src'),
       },
     },
-    base: command === 'build' ? '/info/' : '/', 
-    server: {
-      host: "0.0.0.0",
-      port: 10000,
-      allowedHosts: ["*"],
-    },
-    preview: {
-      host: '0.0.0.0', 
-      port: 10000,
-      allowedHosts: ["*"]
-    }
+    base: '/', // Always use root path for Vercel
   }
 })
-
-
-
