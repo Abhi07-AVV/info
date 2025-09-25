79e50729054699d4aa48724c220f62592dc0fe6c
Abhi07-AVV <abhinavin07@gmail.com>
2025-09-25 17:26:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,14 @@ export default defineConfig(({ command }) => {
       },
     },
     base: command === 'build' ? '/info/' : '/', 
+    server: {
+      host: '0.0.0.0',
+      port: 10000,
+    },
+    preview: {
+      host: '0.0.0.0', 
+      port: 10000,
+    }
   }
 })
+
